505ab8a9372c42801adcac82210811ac8d7b2825
deniskirshenin <deniskirshenin@gmail.com>
2023-10-19 16:14:04 +0400

    fixing collection page types

--- src/app/collection/[handle]/page.tsx
+++ src/app/collection/[handle]/page.tsx
@@ -1,21 +1,18 @@
 import { defaultSort, sorting } from "@/app/lib/constants";
 import { getCollection, getCollectionProducts } from "@/app/lib/shopify";
-import { Product, ShopifyProduct } from "@/app/lib/types";
+import { ShopifyProduct } from "@/app/lib/types";
 import ProductGrid from "@/components/ProductGrid";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-type Props = {
-    params: {handle: string};
-    searchParams: { [key: string]: string | string[] | undefined };
-    products: ShopifyProduct[];
-};
-
-export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
+export async function generateMetadata({
+    params
+  }: {
+    params: { handle: string };
+  }): Promise<Metadata> {
     console.log("params", params);
-    const handle = params.handle;
-    console.log("HANDLE:", handle);
-    const collection = await getCollection(handle);
+    console.log("HANDLE:", params.handle);
+    const collection = await getCollection(params.handle);
 
     if(!collection) return notFound();
 
@@ -29,7 +26,11 @@ const CollectionPage = async ({
     params,
     searchParams,
     products
-}: Props
+}: {
+    params: { handle: string };
+    searchParams: { [key: string]: string | string[] | undefined };
+    products: ShopifyProduct[];
+}
 ) => {
     const { sort } = searchParams as { [key: string]: string};
     const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
